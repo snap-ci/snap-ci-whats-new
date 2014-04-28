58b52fc5b5e3a1701aab5e4de2802c6b637d5ab6
Badrinath Janakiraman <badri.j@gmail.com>
2014-04-28 13:16:09 -0700

    Update 2014_04_24_release.mk

--- changelog/2014_04_24_release.mk
+++ changelog/2014_04_24_release.mk
@@ -1,6 +1,5 @@
-We have a few useful changes in this update that you might want to take action on
+We have a few useful changes in this update
 
-* The Snap URLs for your builds have changed. A build for any repository named "organization/repo-name" in GitHub will now be have its build at /organization/repo-name/branch/branch-name in Snap. Please update any bookmarks and CC-tray URLs.
 * If Snap halted any of your repositories, you can now choose which ones should be left running and which ones you wish to halt. You can do this on <a href="/settings/repositories">the repository listing page</a>.
 * You can repair OAuth connectivity issues with GitHub from the project configuration page for your project.
 * You can repair OAuth connectivity issues with Heroku from the <a href="/settings/heroku_credentials">Heroku credentials page</a>.
