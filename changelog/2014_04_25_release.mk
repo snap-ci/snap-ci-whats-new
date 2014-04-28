1d4f9851e9002ec6522347320217ffdf65a3596d
Badrinath Janakiraman <badri.j@gmail.com>
2014-04-28 13:17:50 -0700

    Create 2014_04_25_release.mk

---
+++ changelog/2014_04_25_release.mk
@@ -0,0 +1,4 @@
+Various Snap URLs have been simplified and changed.
+
+* Builds for a repository named "organization/repo-name" will now be available at "/organization/repo-name/branch/branch-name" in Snap. 
+* Please update any browser bookmarks and CC-tray URLs.
