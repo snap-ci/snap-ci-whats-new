a4103b2752d842e34f54a4c4c858dd122a6036e9
Badrinath Janakiraman <badri.j@gmail.com>
2014-04-28 12:58:44 -0700

    Create 2014_04_21_release.mk

---
+++ changelog/2014_04_21_release.mk
@@ -0,0 +1,4 @@
+You can now customize your Heroku deployments to your heart's content!
+* The "Advanced Heroku" stage (as indeed any stage) has the complete Heroku toolbelt available on the command line.
+* As long as you already have created your application either in an earlier stage or using the Heroku dashboard, you can run any and all heroku commands to do things such as enable/disable maintenance mode for your application etc.
+* Beware of the heroku run command as it returns the exits status of the heroku command locally and not on your dyno!
