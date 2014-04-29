3efe99e96a46222799c532f00e3b091da783d11c
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2014-04-29 07:39:42 -0300

    Update 2014_04_25_release.mk

--- changelog/2014_04_25_release.mk
+++ changelog/2014_04_25_release.mk
@@ -1,4 +1,4 @@
-Various Snap URLs have been simplified and changed.
+We have simplified and changed some URLs in Snap.
 
-* Builds for a repository named "organization/repo-name" will now be available at "/organization/repo-name/branch/branch-name" in Snap. 
-* Please update any browser bookmarks and CC-tray URLs.
+* Builds for a repository named "user-name/repo-name" will now be available at "user-name/repo-name/branch/branch-name" in Snap.
+* Please update any browser bookmarks and CCTray URLs.
