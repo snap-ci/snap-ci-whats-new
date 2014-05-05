35d1cce9eb20543b92a0681a6513ba239a6ed2b9
marano <thiagomarano@gmail.com>
2014-05-05 09:49:30 -0300

    Removes unused stuff from processor.

--- lib/processor.rb
+++ lib/processor.rb
@@ -9,14 +9,4 @@ class Processor
     File.open(File.join(to_dir, 'changelog.json'), 'w') { |f| f.write({ changelog: changelog_data }.to_json) }
     File.open(File.join(to_dir, 'latest_changes.json'), 'w') { |f| f.write({ changelog: [changelog_data.first].compact }.to_json) }
   end
-
-  private
-
-  def output_filename(changelog)
-    "#{changelog.year}_#{format_date(changelog.month)}_#{format_date(changelog.day)}_#{changelog.title.gsub(' ', '_')}.html"
-  end
-
-  def format_date(date)
-    date.to_s.rjust(2, "0")
-  end
 end
