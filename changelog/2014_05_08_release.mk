760687768276ce05c8312772616d47dfae96331a
Sahil Muthoo <sahil.muthoo@gmail.com>
2014-05-08 16:44:06 -0300

    Changelog for today

---
+++ changelog/2014_05_08_release.mk
@@ -0,0 +1,13 @@
+Multiple language support has landed on Snap!
+
+* Polygot projects are more than welcome. For example you can choose Ruby and Java on the same project.
+* Please navigate to your project build plan to add extra languagues to existing projects.
+
+We have improved handling of Heroku and GitHub connectivity issues.
+
+* Any error connecting to Heroku or GitHub is flagged on your pipeline's build history.
+* This works well with the repair OAuth with Heroku and GitHub feature we released a couple of weeks ago.
+
+Gem release stages display the version of the gem deployed to RubyGems.
+
+Our favicon changes to a mini build indicator on the build history page. You can keep an eye on your build without switching tabs!
