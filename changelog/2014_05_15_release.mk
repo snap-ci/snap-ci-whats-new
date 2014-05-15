e141767e5937b8a3ac6b871bd720f91aeae926bd
Fernando <fernandoasfj@gmail.com>
2014-05-15 19:34:29 -0300

    Fernakhay Make it pretty

--- changelog/2014_05_15_release.mk
+++ changelog/2014_05_15_release.mk
@@ -1,6 +1,3 @@
-Ruby v2.1.2 is now available. To start using it just go to your project configuration page then choose on the dropdown.
-
-
 It's easier to share with Snap!
 
 * You can now tweet a link to your project directly from your dashboard.
@@ -11,3 +8,4 @@ We want to hear from you!
 * The Snap team values your opinion, and we want to know more about it.
 * We are running some quick surveys, please take a few seconds of your time to answer it. 
 
+Ruby v2.1.2 is now available. To start using it just go to your project configuration page then choose on the dropdown.
