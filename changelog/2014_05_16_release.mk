22460ef635ee9828a7d61a2964c5c5c8aa78df71
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2014-05-16 16:37:22 -0300

    Update 2014_05_16_release.mk

--- changelog/2014_05_16_release.mk
+++ changelog/2014_05_16_release.mk
@@ -1,4 +1,4 @@
 This time around we're just fixing bugs!
 
 * Fixed some bugs with "Rebuild Last Revision" building the oldest commit in the latest pipeline instead of the newest commit.
-* Fixed some bugs with build plan edit page.
+* Fixed some bugs withediting a build plan to add multiple stages at a time.
