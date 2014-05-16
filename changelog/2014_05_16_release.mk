7993d3a71aa48e0deec353599b43c5ef4bb7ea2f
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2014-05-16 16:35:54 -0300

    Create 2014_05_16_release.mk

---
+++ changelog/2014_05_16_release.mk
@@ -0,0 +1,4 @@
+This time around we're just fixing bugs!
+
+* Fixed some bugs with "Rebuild Last Revision" building the oldest commit in the latest pipeline instead of the newest commit.
+* Fixed some bugs with build plan edit page.
