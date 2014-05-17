8de56e03477caffc0a9f792b43c1848b253ea2fc
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2014-05-17 08:04:38 -0300

    Add ability to preview changes locally

--- lib/processor.rb
+++ lib/processor.rb
@@ -8,5 +8,24 @@ class Processor
 
     File.open(File.join(to_dir, 'changelog.json'), 'w') { |f| f.write({ changelog: changelog_data }.to_json) }
     File.open(File.join(to_dir, 'latest_changes.json'), 'w') { |f| f.write({ changelog: [changelog_data.first].compact }.to_json) }
+
+    unless ENV['SNAP_CI']
+      File.open(File.join(to_dir, 'changelog.html'), 'w') do |f|
+        changelog_data.each do |cl|
+          f.puts "<article>"
+          f.puts "<h1>#{cl[:date]}</h1>"
+          f.puts cl[:body]
+          f.puts "</article>"
+        end
+      end
+
+      File.open(File.join(to_dir, 'latest_changes.html'), 'w') do |f|
+        cl = changelog_data.first
+        f.puts "<article>"
+        f.puts "<h1>#{cl[:date]}</h1>"
+        f.puts cl[:body]
+        f.puts "</article>"
+      end
+    end
   end
 end
