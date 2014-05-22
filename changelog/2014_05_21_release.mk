e2395972e6ef7ed1172601519dff61aef5e7fad0
Akshay Karle <akshay.a.karle@gmail.com>
2014-05-22 18:48:41 -0300

    Create 2014_05_21_release.mk

---
+++ changelog/2014_05_21_release.mk
@@ -0,0 +1,9 @@
+We're now building support for Node.js!
+
+* You can now go to your project configuration page and add your favorite node version to the build.
+* We continue to build support for Node.js. We are currently building detection for Node, so that setting up a Node project
+becomes easier. If there is something you would like to see in Node JS projects, we would like to <a href="/contact-us">hear from you</a>.
+
+We've pushed some bug fixes that prevented you from adding a trailing slash to your artifacts.
+
+The build history page now has the favicon that shows you the state of the last pipeline.
