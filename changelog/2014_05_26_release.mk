fd52bd2727d3aa81327236c783fc7e99459fe91f
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2014-05-26 16:06:54 -0300

    Improve notes

--- changelog/2014_05_26_release.mk
+++ changelog/2014_05_26_release.mk
@@ -1,9 +1,13 @@
-Re-run older builds is now supported!
+We have a few useful changes in this update:
+
+Miscellaneous Improvements:
 
 * Snap will let you re-run any previous build.
 * The "rerun last pipeline" link has been removed.
+* Grunt, Gulp and Bower are now installed and available.
 
-Improvements to tests that need X-Server (selenium etc):
+Browser Tests and X-Server:
 
-* We have improved how browser based tests run.
-* All your tests run in an XVfb server, and we make a `DISPLAY` variable for ease of use.
+* All your tests run in an Xvfb server, and we make a `DISPLAY` variable for ease of use.
+* If you have any `xvfb-run` commands in your build, you may safely remove them.
+* Users who were using `--no-sandbox` for chromedriver may now remove that flag.
