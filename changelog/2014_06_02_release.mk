2c70612747182f57afb5d572e1fd526229866bee
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2014-06-02 17:41:57 -0300

    Tweaks

--- changelog/2014_06_02_release.mk
+++ changelog/2014_06_02_release.mk
@@ -1,8 +1,8 @@
 ### Pipeline parallelization!
 
-* Pipelines are capable of running in parallel. For more information about this feature <a href="http://docs.snap-ci.com/speeding_up_builds/pipeline_parallelism/" target='_blank'>Click here</a>
+* Pipelines are capable of running in parallel. For more information about this feature <a href="http://docs.snap-ci.com/speeding_up_builds/pipeline_parallelism/" target='_blank'>click here</a>
 * This feature requires that you setup your pipeline with setup tasks.
 
 ### Stage/Deployment history
 
-Snap now gives you the ability to view the history of a particular stage. This will let you visualize when a particular stage ran and if there are any pending commits on that stage. <a href='https://snap-ci.com/snap-ci/docs.snap-ci.com/branch/master/stage_history/Production'>Click here</a> to see an example of stage history visualization.
+* Snap now gives you the ability to view the history of a particular stage. This will let you visualize when a particular stage ran and if there are any pending commits on that stage. <a href='https://snap-ci.com/snap-ci/docs.snap-ci.com/branch/master/stage_history/Production'>Click here</a> to see an example of stage history visualization.
