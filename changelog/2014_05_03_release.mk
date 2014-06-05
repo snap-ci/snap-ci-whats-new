e07e0a491e9f6240705392ea04b78a4e53c9e4f3
marano <thiagomarano@gmail.com>
2014-06-05 13:41:47 -0300

    Progress bar news.

---
+++ changelog/2014_05_03_release.mk
@@ -0,0 +1,4 @@
+Build history now has a progress bar for improved activity feedback!
+
+* The new bar indicates the current progress of each running stage.
+* The expected time the stage will take to complete is calculated taking into account previous successfull runs.
