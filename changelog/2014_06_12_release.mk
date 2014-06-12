76cab254f0e36003dd3092ccc55759a1469b1e07
Akshay Karle <akshay.a.karle@gmail.com>
2014-06-12 11:49:08 -0300

    Update about jruby

---
+++ changelog/2014_06_12_release.mk
@@ -0,0 +1,6 @@
+JRuby is in!
+
+Snap now supports JRuby. You can select your favorite version from the Ruby language drop down on the configuration page.
+To check the versions Snap supports <a href="http://docs.snap-ci.com/supported_platforms/">click here</a>.
+
+If you are passionate about JRuby and would like help us improve the detection or recipes please <a href="/contact-us">contact us</a>.
