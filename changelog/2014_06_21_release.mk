51a5a2b1242243d31336e1806c1ae84338e58d4c
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2014-06-20 13:32:49 -0300

    tweak a message

--- changelog/2014_06_21_release.mk
+++ changelog/2014_06_21_release.mk
@@ -1,6 +1,6 @@
 Here are some things we released:
 
 * Upgrades to google chrome and chromedriver to the latest and greatest version, this has been a long pending change
-* Fix a bug where some users were receiving email notifications from repositories they were not watching.
+* Fix a bug where some users were receiving email notifications from repositories they were not watching, but collaborating on.
 
 We want to hear from you! If you have any feedback or features you would love to see in Snap, <a href="http://snap-ci.com/contact-us">click here</a> and let us know.
