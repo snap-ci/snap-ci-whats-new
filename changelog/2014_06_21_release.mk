9e94ab5f833b3b0137cb4f00f63d88bbc3b9dd86
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2014-06-20 14:11:56 -0300

    Add a note on QT upgrade

--- changelog/2014_06_21_release.mk
+++ changelog/2014_06_21_release.mk
@@ -1,6 +1,9 @@
 Here are some things we released:
 
-* Upgrades to google chrome and chromedriver to the latest and greatest version, this has been a long pending change
+* Upgrades to google chrome and chromedriver to the latest and greatest version, this has been a long pending change.
+* Upgrade the version of QT from 4.8 to 5.2.
 * Fix a bug where some users were receiving email notifications from repositories they were not watching, but collaborating on.
+
+Find out more about what is [installed on build machines](http://docs.snap-ci.com/supported_platforms/)provided by Snap.
 
 We want to hear from you! If you have any feedback or features you would love to see in Snap, <a href="http://snap-ci.com/contact-us">click here</a> and let us know.
