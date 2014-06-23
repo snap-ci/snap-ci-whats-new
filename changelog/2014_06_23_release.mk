6e0d29a019bdd119c64448b02dae018af5d4899f
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2014-06-23 12:01:51 -0300

    Change the date, and some tweaks

--- changelog/2014_06_23_release.mk
+++ changelog/2014_06_23_release.mk
@@ -2,8 +2,9 @@ Here are some things we released:
 
 * Upgrades to google chrome and chromedriver to the latest and greatest version, this has been a long pending change.
 * Upgrade the version of QT from 4.8 to 5.2.
-* Upgrade the version of bundler from 1.5.2 to 1.6.3. You may use `bundle install -j4` to speed up builds.
+* Upgrade the version of bundler from 1.5.2 to 1.6.3. You may use `bundle install -j4` to speed up builds. You may use `bundle install -j4 --path $HOME/.bundle` to futher improve speeds further.
 * Fix a bug where some users were receiving email notifications from repositories they were not watching, but collaborating on.
+* Improved the project setup experience.
 
 Find out more about what is [installed on build machines](http://docs.snap-ci.com/supported_platforms/) provided by Snap.
 
