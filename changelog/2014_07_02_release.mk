fba75cda6c6268a1cd86f1c3aef6ca739bc5b978
marano <thiagomarano@gmail.com>
2014-07-03 13:57:50 -0300

    Some additions.

--- changelog/2014_07_02_release.mk
+++ changelog/2014_07_02_release.mk
@@ -1,10 +1,11 @@
 Faster builds
 
 * We improved the time Snap takes to setup your build, so they will now take less time to start running.
+* Build history improved feedback time.
 * Build history page now shows *scheduled* and *cloning* states, so you have a better sense of what is going in the first moments of each pipeline stage.
 
 An all brand new Pipeline Configuration page
 
 * Cleaner interface.
-* Intuitive edition.
+* Intuitive editing.
 * Takes less clicks to configure a pipeline.
