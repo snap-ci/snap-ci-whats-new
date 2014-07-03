a13c8821582ccc84af24767f734645408e7fe0e0
marano <thiagomarano@gmail.com>
2014-07-03 13:54:08 -0300

    Feel fixes.

--- changelog/2014_07_02_release.mk
+++ changelog/2014_07_02_release.mk
@@ -1,7 +1,7 @@
 Faster builds
 
-* We improved the time Snap takes to setup your build, so it will now take less time to start running.
-* Build history page now shows *scheduled* and *cloning*, so you have a better sense of what is going on at the first moments of each pipeline stage.
+* We improved the time Snap takes to setup your build, so they will now take less time to start running.
+* Build history page now shows *scheduled* and *cloning* states, so you have a better sense of what is going in the first moments of each pipeline stage.
 
 An all brand new Pipeline Configuration page
 
