d73e676f4bcecbfd24c23ce7b6e1ed7bd0020cb1
marano <thiagomarano@gmail.com>
2014-07-03 13:49:05 -0300

    Release for new pipeline configuration page and container setup improvements.

---
+++ changelog/2014_07_02_release.mk
@@ -0,0 +1,10 @@
+Faster builds
+
+* We improved the time Snap takes to setup your build, so it will now take less time to start running.
+* Build history page now shows *scheduled* and *cloning*, so you have a better sense of what is going on at the first moments of each pipeline stage.
+
+An all brand new Pipeline Configuration page
+
+* Cleaner interface.
+* Intuitive edition.
+* Takes less clicks to configure a pipeline.
