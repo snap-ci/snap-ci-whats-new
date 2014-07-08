0a85ce35190c4369ff32615c5da1de188570c843
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2014-07-08 13:09:03 -0300

    Remove an excess period

--- changelog/2014_07_09_release.mk
+++ changelog/2014_07_09_release.mk
@@ -1,6 +1,6 @@
 PHP is in!
 
-Snap now supports PHP. You can select your favorite version from the PHP language drop down on the configuration page. To check the versions Snap supports [click here](http://docs.snap-ci.com/the_ci_environment/languages/#php)..
+Snap now supports PHP. You can select your favorite version from the PHP language drop down on the configuration page. To check the versions Snap supports [click here](http://docs.snap-ci.com/the_ci_environment/languages/#php).
 
 Snap now detects when your build runs out of memory. We'll send you an email when this happens. You now have the option to get in touch with us and request more resources to run your build.
 
