83ff3a0308da5c5e2175b229ef4418867f16bc4a
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2014-07-08 12:58:08 -0300

    Add whats new

---
+++ changelog/2014_07_09_release.mk
@@ -0,0 +1,11 @@
+PHP is in!
+
+Snap now supports PHP. You can select your favorite version from the PHP language drop down on the configuration page. To check the versions Snap supports [click here](http://docs.snap-ci.com/the_ci_environment/languages/#php)..
+
+Snap now detects when your build runs out of memory. We'll send you an email when this happens. You now have the option to get in touch with us and request more resources to run your build.
+
+Some bug fixes:
+
+* Some users have had issues with Snap being unable to handle periods "." in branch names, we have fixed this.
+
+We want to hear from you! If you have any feedback or features you would love to see in Snap, [click here](http://snap-ci.com/contact-us) and let us know.
