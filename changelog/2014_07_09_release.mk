2e0fef28ede25c99808d2e9aa34c5947264b489f
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2014-07-08 13:04:18 -0300

    Add documentation about new environment variables

--- changelog/2014_07_09_release.mk
+++ changelog/2014_07_09_release.mk
@@ -4,8 +4,12 @@ Snap now supports PHP. You can select your favorite version from the PHP languag
 
 Snap now detects when your build runs out of memory. We'll send you an email when this happens. You now have the option to get in touch with us and request more resources to run your build.
 
-Some bug fixes:
+We now provide a few more environment variables for your builds:
 
-* Some users have had issues with Snap being unable to handle periods "." in branch names, we have fixed this.
+* `TRACKING_PIPELINE` - set to `true` if the pipeline is a tracking pipeline
+* `INTEGRATION_PIPELINE` - set to `true` if the pipeline is an integration pipeline
+* `SNAP_CACHE_DIR` - if you store any files here, we'll try our best to keep it around across pipeline runs
+
+Some users have had issues with Snap being unable to handle periods "." in branch names, we have fixed this.
 
 We want to hear from you! If you have any feedback or features you would love to see in Snap, [click here](http://snap-ci.com/contact-us) and let us know.
