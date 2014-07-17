80bce2901675b3af5bc5a804ae5a80e00af07f1a
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2014-07-17 08:57:57 -0300

    Add a what's new about sudo access

---
+++ changelog/2014_07_17_release.mk
@@ -0,0 +1,10 @@
+sudo make me a sandwich, aka moar sudo.
+
+Snap already provided sudo access to install your packages using `yum`, `gem` and `npm`.
+
+* Since we rolled out PHP last week, we now offer sudo access for `pear`, `peardev` and `pecl`.
+* Few people asked to install services, so Snap now offers sudo access to `/etc/init.d/YOURSERVICE [start|stop|restart]` and `service YOURSERVICE [start|stop|restart]`
+
+Some users have had issues with editing of their build configuration, we have fixed all of these issues.
+
+We want to hear from you! If you have any feedback or features you would love to see in Snap, [click here](http://snap-ci.com/contact-us) and let us know.
