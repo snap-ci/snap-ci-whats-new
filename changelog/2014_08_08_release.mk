93ae3edf14fe50a45fe6a93c63f5d6df96119db1
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2014-08-08 10:02:31 -0300

    Add a whats new for aug-08 release

---
+++ changelog/2014_08_08_release.mk
@@ -0,0 +1,15 @@
+It's easier to diagnose your builds on Snap!
+
+* We added a command `snap-shell` that will assist with diagnosing problems with your build. Using this command as part of your project configuration will provide web based command line when you visit the build history page. Read more about it on [our blog](http://blogs.snap-ci.com/blog/2014/08/01/introducing-snap-shell/).
+
+Where's my API?
+
+* Many users asked us for API access to be able to trigger builds. Snap now offers the ability to trigger manual stages, and new pipelines. Read more about it on [our docs](http://docs.snap-ci.com/api/introduction/).
+
+Secure environment variables
+
+* Snap now offers you to provide us [secure environment variables](http://docs.snap-ci.com/pipeline/#environment-variables). These variables are encrypted before being saved. We never show this value either when the project configuration is edited or if it is accidentally printed in the console log.
+
+Fun stuff!
+
+* We neither confirm nor deny the presence of konami code on our website.
