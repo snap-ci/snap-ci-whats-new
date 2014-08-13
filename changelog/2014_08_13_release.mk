47861be010f1d65a77d4e18f726c4d26b497c02d
Akshay Karle <akshay.a.karle@gmail.com>
2014-08-13 15:14:10 -0300

    secure files and artifacts

---
+++ changelog/2014_08_13_release.mk
@@ -0,0 +1,4 @@
+Ever wanted to upload sensitive files to Snap?
+
+Snap now supports uploading of files to stages. If you've ever wanted to upload your ssh keys, certs, config files to Snap you can
+do it using our file upload feature. Take a look at our [docs](http://docs.snap-ci.com/pipeline/#secure-files) for more details.
