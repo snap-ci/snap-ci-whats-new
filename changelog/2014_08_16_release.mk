b64d0778d94fe875278df461d10264bda03df9e3
Leandro Gualter <leandrogualter@gmail.com>
2014-08-15 15:58:15 -0300

    Fix typo

--- changelog/2014_08_16_release.mk
+++ changelog/2014_08_16_release.mk
@@ -1,8 +1,12 @@
 Now it's easier to PHP on Snap!
 
 * You can now use the command `add-php-configuration` to add custom configuration ini files! Just add a command to your stage containing:
-      add-php-configuration &lt;path to config ini file&gt;
+
+    `add-php-configuration &lt;path to config ini file&gt;`
+
 * And for enabling extensions we now provide the command `enable-php-extension`. Add it to your stage:
-      enable-php-extension &lt;extension name&gt;
+
+    `enable-php-extension &lt;extension name&gt;`
+
 * In addition to these changes we also added support for the version 5.6!
 * Check our [docs](http://docs.snap-ci.com/the_ci_environment/languages/#php) for more info.
