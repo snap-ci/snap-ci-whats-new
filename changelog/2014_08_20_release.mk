a02596926cde918e64e0b17b6b4a83278a6c5f4c
Badrinath Janakiraman <badri.j@gmail.com>
2014-08-20 17:22:44 -0300

    Update 2014_08_20_release.mk

--- changelog/2014_08_20_release.mk
+++ changelog/2014_08_20_release.mk
@@ -2,14 +2,13 @@ More love for artifacts
 
 * You may now specify glob patterns as artifacts. Examples such as `target/*.jar` or `target/**/*.{deb,rpm}` will now work in Snap.
 * Snap now allows files as artifacts.
+* Read more about artifacts in our [documentation](http://docs.snap-ci.com/pipeline/#artifact)
 
-Read more about artifacts in our [documentation](http://docs.snap-ci.com/pipeline/#artifact)
-
-Even moar `sudo`!
+Moar `sudo`!
 
 * Snap earlier allowed limited `sudo` access for installing packages and managing services. We now offer you the convenience and power of full sudo access.
 * You can now configure your build machine to your heart's content. Need extensive configuration - just throw in chef/puppet command in your configuration!
 
-More freedom in defining stage names
+Little things, nice things and bug fixes
 
 * Stage names can now have numbers, hyphens and underscores. Examples are: `Fast_Feedback-1`, `Smoke-Deploy` and so on.
