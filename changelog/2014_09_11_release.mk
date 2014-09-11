48b3f7bb133ed73e38db6da298345e4624096051
Akshay Karle <akshay.a.karle@gmail.com>
2014-09-11 15:14:12 -0300

    build plan edit tips and bug fixes

---
+++ changelog/2014_09_11_release.mk
@@ -0,0 +1,8 @@
+Tips on Build configuration edits
+
+* We now provide tips for various popular commands which may help in speeding up your builds, gotchas you might have when using them, etc.
+
+Bug bash time!
+
+* Fixed an issue on the build history page where it would not update how long ago the stage ran.
+* Fixed the auto-tailing of console logs on Firefox.
