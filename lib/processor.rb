3b27658c09f015e3cfef5e3842a7edea38267f08
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2015-04-06 11:46:01 +0530

    Only render the top 10 changelog entries

--- lib/processor.rb
+++ lib/processor.rb
@@ -4,7 +4,7 @@ class Processor
   def run(from_dir, to_dir)
     changelog_data = Rollin::Blog.new(articles_folder: from_dir).articles.map do |changelog|
       { date: changelog.date, body: changelog.body }
-    end
+    end.first(10)
 
     File.open(File.join(to_dir, 'changelog.json'), 'w') { |f| f.write({ changelog: changelog_data }.to_json) }
     File.open(File.join(to_dir, 'latest_changes.json'), 'w') { |f| f.write({ changelog: [changelog_data.first].compact }.to_json) }
